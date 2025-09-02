0ba7ec099f6371d21c269afe5b016fb5299d1cdf
gamejam-sandwich <pinkelephantloopsy@gmail.com>
2025-09-01 23:49:11 -0700

    Setting up basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+export default function App() {
+  let totalCalories = 0;
+  /* FUNCTIONS
+    onclick for logging a food
+    onclick for deleting logs later
+    addfood(event): unpack data from the form into array
+    update calorie total
+  */
+  function handleClick() {
+
+  }
+
+  function handleInputChange(ev) {
+
+  }
+
   return (
-    <span>Hello</span>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      {/*
+      title, calorie count
+      --------------------
+      btn to log food,
+      input to write food name & calories,
+      selector for which meal,
+      empty div to contain list of foods,
+      */}
+      <h1>om nom nom</h1>
+      <p>{totalCalories} calories</p>
+      <br />
+      <div className="input-container">
+        <input placeholder="Ex: bologne" onInput={handleInputChange}></input>
+        <input placeholder="Ex: 150" onInput={handleInputChange}></input>
+        <select>
+          <option>Breakfast</option>
+          <option>Lunch</option>
+          <option>Dinner</option>
+        </select>
+        <button onClick={handleClick}>om nom nom</button>
+      </div>
+
+    </div>
   )
 }
 
-export default App
