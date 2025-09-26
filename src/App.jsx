4a51c3411fe08b7853dbd954cd69b249d400163f
gamejam-sandwich <pinkelephantloopsy@gmail.com>
2025-09-26 00:01:17 -0700

    modified:   src/App.css 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
+import { useState, useRef } from "react";
 import './App.css'
 
 export default function App() {
-  let totalCalories = 0;
-  /* FUNCTIONS
-    onclick for logging a food
-    onclick for deleting logs later
-    addfood(event): unpack data from the form into array
-    update calorie total
-  */
-  function handleClick() {
+  // States and variables
+  const [name, setName] = useState("");
+  const [calories, setCalories] = useState("");
+  const [foodList, setFoodList] = useState([]);
+  const inputRef = useRef(null);
+  const totalCalories = foodList.reduce((total, food) => {
+    return total + Number(food.calories || 0);
+  }, 0);
 
+  // Add information from user input into foodList
+  // Empty textboxes to ready for next input
+  function addFood(ev) {
+    ev.preventDefault();
+    const food = { name, calories };
+    // Check input value of calories (whole/valid number)
+    // Check that food input isn't blank
+    if (isNaN(+calories) || calories.includes('.')) {
+      alert("Please input a valid number.");
+      return;
+    }
+    if (name.trim() === "") {
+      alert("Please input a food name.");
+      return;
+    }
+
+    setFoodList(prevList => [...prevList, food]);
+    setName("");
+    setCalories("");
+    inputRef.current.focus();
   }
 
-  function handleInputChange(ev) {
+  // Render foods in foodList, most recent entry appears on top
+  function renderFoodList() {
+    const options = [];
+    const reversedFoodList = [...foodList].reverse();
+    for (let i = 0; i < reversedFoodList.length; i++) {
+      options[i] = (
+        <option key={i}>
+          {reversedFoodList[i].name}: {reversedFoodList[i].calories}
+        </option>
+      );
+    }
 
+    return options;
   }
 
   return (
@@ -24,28 +55,35 @@ export default function App() {
         flexDirection: "column"
       }}
     >
-      {/*
-      title, calorie count
-      --------------------
-      btn to log food,
-      input to write food name & calories,
-      selector for which meal,
-      empty div to contain list of foods,
-      */}
       <h1>om nom nom</h1>
-      <p>{totalCalories} calories</p>
+      <p>Calories: {totalCalories}</p>
       <br />
-      <div className="input-container">
-        <input placeholder="Ex: bologne" onInput={handleInputChange}></input>
-        <input placeholder="Ex: 150" onInput={handleInputChange}></input>
+      <div className="container">
+        <input
+          ref={inputRef}
+          placeholder="Ex: bologne"
+          value={name}
+          onChange={(e) => setName(e.target.value)}>
+        </input>
+        <input
+          placeholder="Ex: 150"
+          value={calories}
+          onChange={(e) => setCalories(e.target.value)}>
+        </input>
+        {/*for later
+       
         <select>
           <option>Breakfast</option>
           <option>Lunch</option>
           <option>Dinner</option>
         </select>
-        <button onClick={handleClick}>om nom nom</button>
+      */}
+        <button onClick={addFood}>
+          Eat!
+        </button>
       </div>
 
+      <div className="food-list">{renderFoodList()}</div>
     </div>
   )
 }
